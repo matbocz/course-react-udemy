7192307e4d59cfc7606ffe23556ec978b234f274
Mateusz Boczarski <matbocz@outlook.com>
2020-07-09 20:36:09 +0200

    Create Cart_ver2

---
+++ Cart_ver2/app.js
@@ -0,0 +1,58 @@
+class App extends React.Component {
+    state = {
+        availableProducts: 5,
+        shoppingCart: 1,
+    };
+
+    handleRemoveFromCart = () => {
+        this.setState({
+            shoppingCart: this.state.shoppingCart - 1,
+        });
+    };
+
+    handleAddToCart = () => {
+        this.setState({
+            shoppingCart: this.state.shoppingCart + 1,
+        });
+    };
+
+    handleBuy = () => {
+        this.setState({
+            availableProducts:
+                this.state.availableProducts - this.state.shoppingCart,
+            shoppingCart: 0,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <button
+                    disabled={this.state.shoppingCart ? false : true}
+                    onClick={this.handleRemoveFromCart}
+                >
+                    -
+                </button>
+
+                <span> {this.state.shoppingCart} </span>
+
+                <button
+                    disabled={
+                        this.state.shoppingCart === this.state.availableProducts
+                            ? true
+                            : false
+                    }
+                    onClick={this.handleAddToCart}
+                >
+                    +
+                </button>
+
+                {this.state.shoppingCart > 0 && (
+                    <button onClick={this.handleBuy}>Buy</button>
+                )}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
